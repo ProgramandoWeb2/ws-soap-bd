9312e5541c57a440698614e9e9b01b1cd87d8f11
ProgramandoWeb2 <daplicacionesmoviles1@gmail.com>
2024-10-12 20:50:39 -0500

    web service SOAP paises

--- src/main/java/pe/edu/cibertec/ws_soap_bd/config/WebServiceConfig.java
+++ src/main/java/pe/edu/cibertec/ws_soap_bd/config/WebServiceConfig.java
@@ -1,4 +1,41 @@
 package pe.edu.cibertec.ws_soap_bd.config;
 
-public class WebServiceConfig {
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet>
+    messageDispatcherServlet(ApplicationContext applicationContext){
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+    @Bean
+    public XsdSchema paisEsquema(){
+        return new SimpleXsdSchema(new ClassPathResource("xsd/countries.xsd"));
+    }
+
+    @Bean(name = "paises")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema paisEsquema){
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("PaisesPort");
+        wsdl11Definition.setLocationUri("/ws/paises");
+        wsdl11Definition.setTargetNamespace("http://www.cibertec.edu.pe/ws/objects");
+        wsdl11Definition.setSchema(paisEsquema);
+        return wsdl11Definition;
+    }
+
 }
